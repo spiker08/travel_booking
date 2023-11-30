073cb8d919d557d91f6c3802b32ce0951ec90c9e
spiker08 <jkpatel8646@gmail.com>
2023-12-01 04:50:45 +0530

    initial commit

---
+++ src/app.py
@@ -0,0 +1,18 @@
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/booking', methods=['GET', 'POST'])
+def booking():
+    if request.method == 'POST':
+        # Handle form submission and booking logic
+        # Example: booking_data = request.form['booking_data']
+        return render_template('booking.html', booking_data=booking_data)
+    return render_template('booking.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)

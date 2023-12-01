787fd0a5c1b4ba09844b9c1e6617e6359c30cad0
spiker08 <jkpatel8646@gmail.com>
2023-12-01 05:31:46 +0530

    html template changes

--- src/app.py
+++ src/app.py
@@ -1,18 +1,10 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('index.html')
-
-@app.route('/booking', methods=['GET', 'POST'])
-def booking():
-    if request.method == 'POST':
-        # Handle form submission and booking logic
-        # Example: booking_data = request.form['booking_data']
-        return render_template('booking.html')
-    return render_template('booking.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
